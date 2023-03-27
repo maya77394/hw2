956cf020062e5d296302d2019af0cee71348395e
maya77394 <120264572+maya77394@users.noreply.github.com>
2023-03-27 14:53:55 +0300

    Update openMP.C

--- openMP.C
+++ openMP.C
@@ -1,84 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <time.h>
+#include <omp.h>
 
-#define M 500
-#define N 500
-#define P 500
-#define NUM_THREADS 4
-
-int matrix1[M][N], matrix2[N][P], result[M][P];
-
-struct thread_data {
-    int thread_id;
-    int start_row;
-    int end_row;
-};
-
-void *multiply_matrices(void *arg) {
-    struct thread_data *data = (struct thread_data *)arg;
-    int start_row = data->start_row;
-    int end_row = data->end_row;
-    int i, j, k;
-
-    for (i = start_row; i < end_row; i++) {
-        for (j = 0; j < P; j++) {
-            result[i][j] = 0;
-            for (k = 0; k < N; k++) {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
-            }
-        }
-    }
-
-    pthread_exit(NULL);
-}
+#define SIZE 500
 
 int main() {
-    int i, j;
-    pthread_t threads[NUM_THREADS];
-    struct thread_data thread_data_array[NUM_THREADS];
-    clock_t start, end;
-    double cpu_time_used;
+    int i, j, k;
+    int matrix1[SIZE][SIZE], matrix2[SIZE][SIZE], result[SIZE][SIZE];
 
     // Initialize matrices with random values
-    srand(time(NULL));
-    for (i = 0; i < M; i++) {
-        for (j = 0; j < N; j++) {
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
             matrix1[i][j] = rand() % 10;
-        }
-    }
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < P; j++) {
             matrix2[i][j] = rand() % 10;
         }
     }
 
-    // Multiply matrices using pthreads
-    start = clock();
-    int chunk_size = M / NUM_THREADS;
-    for (i = 0; i < NUM_THREADS; i++) {
-        thread_data_array[i].thread_id = i;
-        thread_data_array[i].start_row = i * chunk_size;
-        thread_data_array[i].end_row = (i == NUM_THREADS - 1) ? M : (i + 1) * chunk_size;
-        pthread_create(&threads[i], NULL, multiply_matrices, (void *)&thread_data_array[i]);
+    // Matrix multiplication with OpenMP
+    double start_time = omp_get_wtime();
+    #pragma omp parallel for private(i,j,k) shared(matrix1,matrix2,result)
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            result[i][j] = 0;
+            for (k = 0; k < SIZE; k++) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
     }
-    for (i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    end = clock();
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    double end_time = omp_get_wtime();
 
     // Print resulting matrix
     printf("Resulting matrix:\n");
-    for (i = 0; i < M; i++) {
-        for (j = 0; j < P; j++) {
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
             printf("%d ", result[i][j]);
         }
         printf("\n");
     }
 
-    printf("Time taken by pthreads: %lf seconds\n", cpu_time_used);
+    printf("Elapsed time: %f seconds\n", end_time - start_time);
 
     return 0;
 }
